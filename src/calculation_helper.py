3664309b5a350f558ed4423615457d0f13203bb5
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2018-02-10 11:25:13 +0000

    Update calculation_helper.py

--- src/calculation_helper.py
+++ src/calculation_helper.py
@@ -9,28 +9,34 @@ from sklearn.cluster import KMeans
 
 def normalized_overlap(g, node_1, node_2):
     """
+    Function to calculate the normalized neighborhood overlap.
     """
-    inter = len(set(nx.neighbors(g, node_1)).intersection(set(nx.neighbors(g, node_2))))+1
+    inter = len(set(nx.neighbors(g, node_1)).intersection(set(nx.neighbors(g, node_2)))) + 1
     unio = len(set(nx.neighbors(g, node_1)).union(set(nx.neighbors(g, node_2))))
-    return float(inter)/float(unio)
+    overlap_value = float(inter) / float(unio)
+    return overlap_value
 
 def overlap(g, node_1, node_2):
     """
+    Function to calculate the neighborhood overlap.
     """
-    inter = len(set(nx.neighbors(g, node_1)).intersection(set(nx.neighbors(g, node_2))))+1
+    inter = len(set(nx.neighbors(g, node_1)).intersection(set(nx.neighbors(g, node_2)))) + 1
     return float(inter)
 
 def unit(g, node_1, node_2):
     """
+    Function to calculate the 'unit' weight.
     """
     return 1
 
 def min_norm(g, node_1, node_2):
     """
+    Function to calculate the minimum neighborhood overlap.
     """
     inter = len(set(nx.neighbors(g, node_1)).intersection(set(nx.neighbors(g, node_2))))
     min_norm = min(len(set(nx.neighbors(g, node_1))), len(set(nx.neighbors(g, node_2))))
-    return float(inter)/float(min_norm)
+    min_overlap = float(inter) / float(min_norm)
+    return min_overlap
 
 def overlap_generator(metric, graph):
     """
@@ -42,7 +48,7 @@ def overlap_generator(metric, graph):
 def classical_modularity_calculator(graph, embedding, args):
     """
     """
-    kmeans = KMeans(n_clusters=args.cluster_number, random_state=0, n_init = 1).fit(embedding)
+    kmeans = KMeans(n_clusters=args.cluster_number, random_state=0, n_init=1).fit(embedding)
     assignments = {i: int(kmeans.labels_[i]) for i in range(0, embedding.shape[0])}
-    modularity = community.modularity(assignments,graph)
+    modularity = community.modularity(assignments, graph)
     return modularity, assignments
