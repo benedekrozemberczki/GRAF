832b86d232259839a482c1d05c3cc5a252c4c35e
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2018-02-10 11:20:30 +0000

    Update factorizer.py

--- src/factorizer.py
+++ src/factorizer.py
@@ -1,11 +1,11 @@
 from parser import parameter_parser
 from print_and_read import graph_reader
-from calculation_helper import overlap, overlap_generator, classical_modularity_calculator, normalized_overlap
 from model import Factor
-import numpy as np
-from sklearn.preprocessing import scale
+
 def create_and_run_model(args):
-    
+    """
+    Function to read the graph, create and embedding and train it.
+    """
     graph = graph_reader(args.input)
     model = Factor(args, graph)
     model.train()
@@ -13,6 +13,3 @@ def create_and_run_model(args):
 if __name__ == "__main__":
     args = parameter_parser()
     create_and_run_model(args)
-
-
-
